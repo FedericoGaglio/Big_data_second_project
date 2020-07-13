3d39e237ff30e768b115f3bca46879899e6b04ed
Alessio Verdolini <alessio.verdolini@gmail.com>
2020-07-13 18:01:16 +0200

    update

--- scripts/start-all.sh
+++ scripts/start-all.sh
@@ -3,10 +3,13 @@
 
 echo -e '\n\n################# WAIT INFRASTRUCTURE BOOTSTRAP #################' &&
 docker-compose up -d kafka zookeeper spark-master spark-worker-1 influx grafana &&
-echo -e '\n\n################# INFRASTRUCTURE IS RUNNING #################'
-
-#xdg-open http://localhost:3000/d/covid/covid-dashboard
-open http://localhost:3000/d/covid/covid-dashboard
 
 echo -e '\n\n################# CONFIG INFLUX #################' &&
 bash config-influx.sh &&
+
+echo -e '\n\n################# INFRASTRUCTURE IS RUNNING #################' &&
+
+open http://localhost:3000/d/covid/covid-dashboard
+#xdg-open http://localhost:3000/d/covid/covid-dashboard
+
+
