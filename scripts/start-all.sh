6d6c0d9fafdc779722201595155d6dd6e53aa40e
Alessio Verdolini <alessio.verdolini@gmail.com>
2020-07-14 12:00:05 +0200

    refactr dashboard

--- scripts/start-all.sh
+++ scripts/start-all.sh
@@ -14,13 +14,13 @@ bash producers/bootstrap-all-producer.sh &&
 sleep 5 &&
 
 if [ "$(uname)" == "Darwin" ]; then
-  open http://localhost:3000/d/covid/covid-dashboard
+  open http://localhost:3000/d/covid/Home
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  xdg-open http://localhost:3000/d/covid/covid-dashboard
+  xdg-open http://localhost:3000/d/covid/Home
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    start http://localhost:3000/d/covid/covid-dashboard
+    start http://localhost:3000/d/covid/Home
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    start http://localhost:3000/d/covid/covid-dashboard
+    start http://localhost:3000/d/covid/Home
 fi
 
 
